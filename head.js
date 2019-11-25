358eda7c5de8d1d15b292f3cbe0551bf91d21281
Michael-Xie <xiemicha@gmail.com>
2019-11-25 20:18:03 +0000

    Linted head.js

--- head.js
+++ head.js
@@ -10,7 +10,7 @@ const head = function(arr) {
 };
 
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1]), 1);
 assertEqual(head([]), undefined);
