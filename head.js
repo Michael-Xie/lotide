eec47b661b84f4aad249d0329a7708a65cfa3c17
Michael-Xie <xiemicha@gmail.com>
2019-11-25 20:15:37 +0000

    Added head.js with tests

---
+++ head.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ?
+    `✅ Assertion Passed: ${actual} === ${expected}` :
+    `❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
