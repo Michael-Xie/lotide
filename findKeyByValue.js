fdaeb502688971d2edfc91c7273299dea323805d
Michael-Xie <xiemicha@gmail.com>
2019-11-27 15:34:55 +0000

    Added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ?
+    `✅ Assertion Passed: ${actual} === ${expected}` :
+    `❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(obj, value) {
+  let foundKey;
+
+  for (let key in obj) {
+    if (obj[key] === value) {
+      foundKey = key;
+    }
+  }
+  return foundKey;
+};
+
+// TEST
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
