92f43a8038b7ede558d887cc32f824073a3ed4b2
Michael-Xie <xiemicha@gmail.com>
2019-11-28 15:03:19 +0000

    Added map.js

---
+++ map.js
@@ -0,0 +1,49 @@
+const eqArrays = function(actual, expected) {
+  // false when length of the array is different
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  // false when corresponding elements of actual and expected are different
+  for (let i = 0; i < expected.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const arrToStr = function(arr) {
+  let arrStr = "[";
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      arrStr += `"${arr[i]}"`;
+    } else {
+      arrStr += arr[i];
+    }
+    arrStr = i === arr.length - 1 ? arrStr : arrStr + ", ";
+  }
+  arrStr += "]";
+
+  return arrStr;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  console.log(eqArrays(actual, expected) ?
+    `✅ Assertion Passed: ${arrToStr(actual)} === ${arrToStr(expected)}` :
+    `❌ Assertion Failed: ${arrToStr(actual)} !== ${arrToStr(expected)}`);
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map([], word => word[0]), []);
+assertArraysEqual(map(["hello"], word => word[0]), ["h"]);
