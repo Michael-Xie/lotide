e470d2ce839373f843d6652e1632b530590a6e85
Michael-Xie <xiemicha@gmail.com>
2019-12-02 16:53:34 +0000

    Moved tests from middle.js and made sure it still worked

--- middle.js
+++ middle.js
@@ -1,41 +1,3 @@
-// TEST/ASSERTION/HELPER FUNCTIONS
-const eqArrays = function(actual, expected) {
-  // false when length of the array is different
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  // false when corresponding elements of actual and expected are different
-  for (let i = 0; i < expected.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const arrToStr = function(arr) {
-  let arrStr = "[";
-
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === "string") {
-      arrStr += `"${arr[i]}"`;
-    } else {
-      arrStr += arr[i];
-    }
-    arrStr = i === arr.length - 1 ? arrStr : arrStr + ", ";
-  }
-  arrStr += "]";
-
-  return arrStr;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  console.log(eqArrays(actual, expected) ?
-    `✅ Assertion Passed: ${arrToStr(actual)} === ${arrToStr(expected)}` :
-    `❌ Assertion Failed: ${arrToStr(actual)} !== ${arrToStr(expected)}`);
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   // array with one or two elements return an empty array
   let midArr = [];
@@ -56,10 +18,5 @@ const middle = function(array) {
   return midArr;
 };
 
-// TEST CODE
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
