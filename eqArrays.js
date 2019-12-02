4ea2a6979da13e48d8f929aad06f8b521a5c82be
Michael-Xie <xiemicha@gmail.com>
2019-12-02 15:48:41 +0000

    Moved tests out of eqArrays and tested that things still worked

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  console.log(actual === expected ?
-    `✅ Assertion Passed: ${actual} === ${expected}` :
-    `❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
@@ -23,16 +16,4 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-
-assertEqual(eqArrays([[2, 3], [4], [[[5]]]] , [[2, 3], [4], [[[5]]]]), true); // 4th level array same
-assertEqual(eqArrays([[2, 3], [4], [[[5, 6]]]] , [[2, 3], [4], [[[5]]]]), false); // 4th level array diff
+module.exports = eqArrays;
