4a81de7f12b21f65998bb243d8731600eac35aed
Michael-Xie <xiemicha@gmail.com>
2019-12-02 21:43:00 +0000

    Changed tests to use Mocha Chai for headTest, middleTest, and tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,42 @@
+const assert = require('chai').assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+  it("return empty array when given an empty array", () => {
+    let actual = middle([]);
+    let expected = [];
+    assert.deepEqual(actual, expected);
+  })
+
+  it("return empty array when given a one element array", () => {
+    let actual = middle([1]);
+    let expected = [];
+    assert.deepEqual(actual, expected);
+  })
+
+  it("return empty array when given a two element array", () => {
+    let actual = middle([1, 2]);
+    let expected = [];
+    assert.deepEqual(actual, expected);
+  })
+
+  it("return the middle element when given a odd-number of elements array", () => {
+    let actual = middle([1, 2, 3]);
+    let expected = [2];
+    assert.deepEqual(actual, expected);
+  })
+
+  it("return the middle two elements when given a even-number of elements array", () => {
+    let actual = middle([1, 2, 3, 4]);
+    let expected = [2, 3];
+    assert.deepEqual(actual, expected);
+  })
+})
+
+// assertArraysEqual(middle([]), []);
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,24 +1,104 @@
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const tail = require("../tail");
-const assertEqual = require("../assertEqual");
-// Test Case 1: Check the returned array elements
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case 2: Empty array
-result = tail([]);
-assertEqual(result.length, 0);
+describe("#tail", () => {
+  
 
-// Test Case 3: One element array
-result = tail(["Hello"]);
-assertEqual(result.length, 0);
+  it("returns tail of arr", () => {
+    let result = tail(["Hello", "Lighthouse", "Labs"]);
+    let expected = ["Lighthouse", "Labs"];
+    assert.deepEqual(result, expected);
+  });
 
-// Test Case 4: Original array is not modified
-let arr = ["Hello", "Worldly", "World"];
-result = tail(arr);
-assertEqual(arr.length, 3);
-assertEqual(arr[0], "Hello");
-assertEqual(arr[1], "Worldly");
-assertEqual(arr[2], "World");
+  // it("returns length 2 for the tail of ['Hello', 'Lighthouse', 'Labs']", () => {
+  //   let result = tail(["Hello", "Lighthouse", "Labs"]);
+  //   assert.strictEqual(result.length, 2);
+  // });
+
+  // it("returns for the tail of ['Hello', 'Lighthouse', 'Labs'] at element 0", () => {
+  //   let result = tail(["Hello", "Lighthouse", "Labs"]);
+  //   assert.strictEqual(result[0], "Lighthouse");
+  // });
+
+  // it("returns for the tail of ['Hello', 'Lighthouse', 'Labs'] at element 1", () => {
+  //   let result = tail(["Hello", "Lighthouse", "Labs"]);
+  //   assert.strictEqual(result[1], "Labs");
+  // });
+
+  it("returns empty array when given an empty array", () => {
+    let actual = tail([]);
+    let expected = [];
+    assert.deepEqual(actual, expected);
+  });
+
+  it("returns an empty array when given a one element array", () => {
+    let actual = tail(["Hello"]);
+    let expected = [];
+    assert.deepEqual(actual, expected);
+  });
+
+  // it("returns a length of 3 for original array after using tail", () => {
+  //   let arr = ["Hello", "Worldly", "World"];
+  //   tail(arr);
+  
+  //   assert.strictEqual(arr.length, 3);
+  // });
+
+  // it("returns 'Hello' from original array at element 0 after using tail", () => {
+  //   let arr = ["Hello", "Worldly", "World"];
+  //   tail(arr);
+  
+  //   assert.strictEqual(arr[0], "Hello");
+  // });
+
+  // it("returns 'Worldly' from original array at element 1 after using tail", () => {
+  //   let arr = ["Hello", "Worldly", "World"];
+  //   tail(arr);
+  
+  //   assert.strictEqual(arr[1], "Worldly");
+  // });
+
+  // it("returns 'World' from original array at element 2 after using tail", () => {
+  //   let arr = ["Hello", "Worldly", "World"];
+  //   tail(arr);
+  
+  //   assert.strictEqual(arr[2], "World");
+  // });  it("returns length 2 for the tail of ['Hello', 'Lighthouse', 'Labs']", () => {
+  //   let result = tail(["Hello", "Lighthouse", "Labs"]);
+  //   assert.strictEqual(result.length, 2);
+  // });
+
+  it("returns 'original array has not been modified after using tail", () => {
+    let arr = ["Hello", "Worldly", "World"];
+    tail(arr);
+    let actual = arr;
+    let expected = ["Hello", "Worldly", "World"];
+  
+    assert.deepEqual(actual, expected);
+  });
+
+
+
+});
+// // Test Case 1: Check the returned array elements
+// let result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case 2: Empty array
+// result = tail([]);
+// assertEqual(result.length, 0);
+
+// // Test Case 3: One element array
+// result = tail(["Hello"]);
+// assertEqual(result.length, 0);
+
+// // Test Case 4: Original array is not modified
+// let arr = ["Hello", "Worldly", "World"];
+// result = tail(arr);
+// assertEqual(arr.length, 3);
+// assertEqual(arr[0], "Hello");
+// assertEqual(arr[1], "Worldly");
+// assertEqual(arr[2], "World");
