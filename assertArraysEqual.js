dcddc56207aa7166a45f550868b80dba75ae641c
Michael-Xie <xiemicha@gmail.com>
2019-12-02 16:01:51 +0000

    Moved tests from assertArraysEqual and ensured it still worked

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(actual, expected) {
-  // false when length of the array is different
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  // false when corresponding elements of actual and expected are different
-  for (let i = 0; i < expected.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const arrToStr = function(arr) {
   let arrStr = "[";
@@ -34,8 +22,4 @@ const assertArraysEqual = function(actual, expected) {
     `❌ Assertion Failed: ${arrToStr(actual)} !== ${arrToStr(expected)}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); //false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); //true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); //false
+module.exports = assertArraysEqual;
