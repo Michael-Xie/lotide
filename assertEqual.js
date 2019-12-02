82045232e96762a3fd36ae7c74dade590ed5316e
Michael-Xie <xiemicha@gmail.com>
2019-12-02 15:26:48 +0000

    Separated assertEqual and head from its tests. The tests were moved to a test folder

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,7 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   console.log(actual === expected ?
     `✅ Assertion Passed: ${actual} === ${expected}` :
     `❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 10);
+module.exports = assertEqual;
