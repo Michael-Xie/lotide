cedabe5593817cdb740d07f7a06c55ec1b76b2e3
Michael-Xie <xiemicha@gmail.com>
2019-12-02 21:32:45 +0000

    Removed archaic code from tail.js

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  console.log(actual === expected ?
-    `✅ Assertion Passed: ${actual} === ${expected}` :
-    `❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const tail = function(arr) {
   return arr.slice(1,arr.length);
 };
